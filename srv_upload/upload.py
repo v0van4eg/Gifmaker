238236f193dc2a8305837c128cacb29cb58af071
Калмыков Владимир <v.kalmykov@gradient.ru>
2025-02-24 12:22:08 +0300

    Микросервисное приложение без api gateway

--- srv_upload/upload.py
+++ srv_upload/upload.py
@@ -18,7 +18,7 @@ app.secret_key = 'your_secret_key'  # Секретный ключ для под�
 uploads_root = os.path.join(app.root_path, 'uploads')  # Путь к директории загрузок
 
 # Подключение к Redis
-redis_client = redis.Redis(host='cloud', port=6379, db=0)
+redis_client = redis.Redis(host='redis', port=6379, db=0)
 
 def allowed_file(filename):
     logger.debug(f"Проверка допустимости файла: {filename}")
